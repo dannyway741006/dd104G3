7c8fe9f16cd8948c55cf5b4857f58804d5903bec
gn757541 <gn757541@gmail.com>
2019-12-19 19:46:38 +0800

    no message

--- js/program.js
+++ js/program.js
@@ -1,23 +1,23 @@
 $(function () {
 
-  $('button.program_add_btn a').click(function (e) {
+  $('button.program_add_btn').click(function (e) {
     e.preventDefault();
 
     if (!$(this).hasClass('active')) {
       $(this).addClass('active');
 
-      $('ul.program_add_list').prepend('<li><input type="text"></li>');
-      $('input').focus();
+      $('ul.program_add_list').prepend('<li><input type="text" class="todolist_input"></li>');
+      $('input.todolist_input').focus();
 
-      $('input').bind('keypress', function (e) {
+      $('input.todolist_input').bind('keypress', function (e) {
         if (e.keyCode === 13) {
-          var text = $('input').val();
+          var text = $('input.todolist_input').val();
 
           if (text !== '') {
-            $('input').hide(0, function () {
+            $('input.todolist_input').hide(0, function () {
               $(this).parent().html(text + '<span>x</span>');
               $(this).remove();
-              $('button.program_add_btn a').removeClass('active');
+              $('button.program_add_btn').removeClass('active');
             });
           }
         }
