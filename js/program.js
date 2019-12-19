e944b2eee04499b79e62e2ef2ea41715f2da8d8a
gn757541 <gn757541@gmail.com>
2019-12-19 20:01:55 +0800

    no message

--- js/program.js
+++ js/program.js
@@ -1,34 +1,34 @@
-$(function () {
+// $(function () {
 
-  $('button.program_add_btn').click(function (e) {
-    e.preventDefault();
+//   $('button.program_add_btn').click(function (e) {
+//     e.preventDefault();
 
-    if (!$(this).hasClass('active')) {
-      $(this).addClass('active');
+//     if (!$(this).hasClass('active')) {
+//       $(this).addClass('active');
 
-      $('ul.program_add_list').prepend('<li><input type="text" class="todolist_input"></li>');
-      $('input.todolist_input').focus();
+//       $('ul.program_add_list').prepend('<li class="todolist_li"><input type="text" class="todolist_input"></li>');
+//       $('input.todolist_input').focus();
 
-      $('input.todolist_input').bind('keypress', function (e) {
-        if (e.keyCode === 13) {
-          var text = $('input.todolist_input').val();
+//       $('input.todolist_input').bind('keypress', function (e) {
+//         if (e.keyCode === 13) {
+//           var text = $('input.todolist_input').val();
 
-          if (text !== '') {
-            $('input.todolist_input').hide(0, function () {
-              $(this).parent().html(text + '<span>x</span>');
-              $(this).remove();
-              $('button.program_add_btn').removeClass('active');
-            });
-          }
-        }
-      });
-    }
-  });
+//           if (text !== '') {
+//             $('input.todolist_input').hide(0, function () {
+//               $(this).parent().html(text + '<span>x</span>');
+//               $(this).remove();
+//               $('button.program_add_btn').removeClass('active');
+//             });
+//           }
+//         }
+//       });
+//     }
+//   });
 
-  $(document).on('click', 'li span', function () {
-    $(this).parent().hide(0, function () {
-      $(this).remove();
-    });
-  });
+//   $(document).on('click', 'li span', function () {
+//     $(this).parent().hide(0, function () {
+//       $(this).remove();
+//     });
+//   });
 
-});
+// });
