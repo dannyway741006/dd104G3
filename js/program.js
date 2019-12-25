fdd6a5a6da1909fd9265f6a28539f3485dd7db44
gn757541 <gn757541@gmail.com>
2019-12-25 12:13:09 +0800

    拖曳

--- js/program.js
+++ js/program.js
@@ -207,15 +207,14 @@ var main_content = new Vue({
 })();
 
 
-
 //拖曳
 $(drag);
 
 function drag() {
-  $(".cards_column_body_todo,.cards_column_body_doing, .cards_column_body_done")
+  $(".cards_list_todo,.cards_list_doing,.cards_list_done")
     .sortable({
       connectWith: ".cards_list",
-      stack: ".cards_column_body .cards_list"
-    })
-    .disableSelection();
+      stack: ".cards_column_body .cards_list",
+      revert:true,
+    }).disableSelection();
 }
