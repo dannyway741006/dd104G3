ace01c18ec144f6b7f372ea825c46974ebce7557
gn757541 <gn757541@gmail.com>
2020-01-03 18:59:25 +0800

    no message

--- js/program.js
+++ js/program.js
@@ -1,6 +1,6 @@
 //新增專案
 var main_content = new Vue({
-  el: "#program_all",
+  el: "#app",
   data: {
     open: false,
 
@@ -18,12 +18,10 @@ var main_content = new Vue({
     add_cards_btn_div: true,
 
     cards_list_card_input_box: false,
-    // cards: [],
-    // card_name: "",
 
     invite_add_member_box: false,
 
-    colors: ["#5395DF", "#ff6e6e", "#89d9b2", "#ffb62e", "#c182ff", "#61cdff", "#a6c1ee", "#f8c3cd", "#f9bf45", "#eb7a77", "#86c166"],
+    colors: ["#B6BE9C", "#48A9A6", "#437C90", "#6CA6C1", "#3581B8", "#C38D94", "#C1666B", "#A09CB0", "#9E768F", "#B2967D", "#7C6C77"],
     selectColor: null,
     new_program_choose_color_item: [],
 
@@ -33,6 +31,30 @@ var main_content = new Vue({
     program_text_btn: false,
 
     add_cards_btn: false,
+
+    // calendar_btn:false,
+    // 日曆部分
+    today: {
+      year: 0,
+      month: 0,
+      date: 0,
+      day: 0
+    },
+    calendar: {
+      year: 0,
+      month: 0,
+      date: 0,
+      day: 0,
+      week: ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"][new Date().getMonth()],
+
+    },
+    // 日曆部分--結束
+
+
+    calendar_btn: false,
+
+
+
 
     page: -1,
     history_page: null,
@@ -46,11 +68,11 @@ var main_content = new Vue({
           program_names: this.program_name,
           changeimage: false, //uncheck
           color: this.selectColor,
-          
+
           cards: [],
           card_name: "",
-          
-         
+
+
         });
         this.program_name = "";
         this.selectColor = null;
@@ -66,7 +88,7 @@ var main_content = new Vue({
     change_watched_text() {
       if (this.click_complete_btn == false) { //已完成專案畫面
         this.click_complete_btn = true;
-        
+
         // document.querySelector(".having_program").style.border = '1px solid red';
       } else { //現有專案畫面
         this.click_complete_btn = false;
@@ -80,10 +102,10 @@ var main_content = new Vue({
       this.cards_list_card_input_box = true;
       add_cards_btn = true;
     },
-    add_card(card_name,index) {
-     console.log(this.program[index]);
+    add_card(card_name, index) {
+      //  console.log(index);
       if (this.card_name !== "") {
-        this.program[index].cards.push(card_name);
+        this.programs[index].cards.push(card_name);
         this.card_name = "";
         this.cards_list_card_input_box = false;
         this.add_cards_btn = false;
@@ -95,23 +117,85 @@ var main_content = new Vue({
       this.programs[index].changeimage = !this.programs[index].changeimage
 
       this.history_programs.push(this.programs[index])
-      
+
       this.page = index - 1;
       this.programs.splice(index, 1);
 
       if (this.programs.length != 0 && this.page == -1) {
         this.page = 0;
       }
-// console.log(this.history_page=this.history_programs.length);
-this.history_page=this.history_programs.length-1;
+      // console.log(this.history_page=this.history_programs.length);
+      this.history_page = this.history_programs.length - 1;
     },
     //刪除專案
     delete_program(index) {
       this.history_programs.splice(index, 1)
     },
 
+    // 日曆部分
+    setToday() {
+      // console.log("123");
+      const date = new Date()
+      this.today.year = this.calendar.year = date.getFullYear()
+      this.today.month = this.calendar.month = date.getMonth() // 0~11
+      this.today.date = this.calendar.date = date.getDate()
+      this.today.day = this.calendar.day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date.getDay()]
+      this.calendar.week = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"][new Date().getMonth()]
+    },
+    adjustYear(fix) {
+      this.calendar.year += fix
+    },
+    adjustMonth(fix) {
+      // this.calendar.month += fix 範圍
+      let month = this.calendar.month + fix
+      // let engeng_month
+      if (month > 11) {
+        this.adjustYear(1)
+        this.calendar.month = 0
+        this.calendar.week = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"][0]
+      } else if (month < 0) {
+        this.adjustYear(-1)
+        this.calendar.month = 11
+        this.calendar.week = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"][11]
+        // this.calendar.eng_month=11
+      } else {
+        this.calendar.month = month
+        this.calendar.week = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"][month]
+      }
+
+    },
+  },
+
+  computed: {
+    // 日曆部分
+    calendarFirstDay() {
+      const mDate = new Date(this.calendar.year, this.calendar.month, 1)
+      const date = new Date(this.calendar.year, this.calendar.month, 1 - mDate.getDay())
+      return {
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        date: date.getDate(),
+        day: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date.getDay()],
+      }
+    },
+    calendarMonth() {
+      const data = []
+      let date
+      for (let i = 0; i < 42; i++) {
+        date = new Date(this.calendarFirstDay.year, this.calendarFirstDay.month, this.calendarFirstDay.date + i)
+        data.push({
+          year: date.getFullYear(),
+          month: date.getMonth(),
+          date: date.getDate(),
+          day: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date.getDay()],
+
+        })
+      }
+      return data
+    }
 
   },
+
 
   mounted() {
 
@@ -123,7 +207,7 @@ this.history_page=this.history_programs.length-1;
       this.invite_add_member_box = false;
       this.setting_btn = false;
       this.add_cards_btn = false;
-
+      this.calendar_btn = false;
       // console.log(this.programs.length - 1)
       if (this.programs.length == 0) {
         // console.log(this.programs.length-1)
@@ -140,9 +224,10 @@ this.history_page=this.history_programs.length-1;
 
     });
 
+    // 日曆部分
+    this.setToday()
 
-    // calender(this.$refs.outCalender);
-    // calender(this.$refs.inCalender);
+
   },
   watch: {
 
