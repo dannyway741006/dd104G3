1be1c45cfde737db9f7d47677dd1086080191223
ezey-lin <4a252099@gamil.com>
2020-01-03 13:40:24 +0800

    no message

--- js/tomato.js
+++ js/tomato.js
@@ -2,12 +2,13 @@ var app = new Vue({
     el:'#tomatoContent',
     data:{
         newTodo:'',
-        countdownTime:'5:00',
+        countdownTime:'1:00',
         start_time: '',
         end_time:'',
         now_time:'',
-        isRest:false,
+        stoping:false,
         countDownTimer:'',
+        t:'',
         todos:[
             {
                 id:'mask1',
@@ -36,61 +37,75 @@ var app = new Vue({
         removeTodo(key){
             this.todos.splice(key,1)    
         },
-        changePlay(key){  
-        if(this.todos[key].isPlay){
-            this.todos[key].isPlay = !this.todos[key].isPlay;
-        }else{
-            this.todos.forEach(item => {
-                item.isPlay = false;
-            });
-            this.todos[key].isPlay = true;
-            this.countDown();
-        }
-        },
-         countDown(){
-            var setMin = this.isRest ? 3 : 2;
-            if(this.start_time == '' && this.end_time==''){
-                this.start_time = new Date();
-                this.end_time = new Date();
-                this.end_time.setMinutes(this.start_time.getMinutes() + setMin);
-                console.log(this.end_time)
-                this.countDownTimer = setTimeout(() => {
-                    this.runCountDown();
-                }, 10);
+        // changePlay(key){  
+        // if(this.todos[key].isPlay){
+        //     this.todos[key].isPlay = !this.todos[key].isPlay;
+        // }else{
+        //     console.log(key)
+        //     this.todos.forEach(item => {
+        //         item.isPlay = false;
+        //     });
+        //     this.todos[key].isPlay = true;
+        //     this.countDown(key);
+        // }
+        // },
+         countDown(key){
+             //暫停
+            if(this.todos[key].isPlay){
+                this.todos[key].isPlay = !this.todos[key].isPlay;
+                
+            }else{
+            //啟動
+                console.log(key)
+                this.todos.forEach(item => {
+                    item.isPlay = false;
+                });
+                this.todos[key].isPlay = true;
+                var setMin = this.stoping ? 3 : 5;
+                if(this.start_time == '' && this.end_time==''){
+                    this.start_time = new Date();
+                    this.end_time = new Date();
+                    this.end_time.setSeconds(this.start_time.getSeconds() + setMin);
+                    console.log(this.end_time)
+                    this.countDownTimer = setTimeout(() => {
+                        this.runCountDown(key);
+                    }, 10);
+                }
             }
+           
         },
-        runCountDown(){
+        runCountDown(key){
             this.now_time = new Date();
             var timeGap = this.end_time-this.now_time;
-            console.log(timeGap);
+            // console.log(timeGap);
             var sec = timeGap % (60 * 1000);
             // console.log(sec);
             if(sec.toString().length > 3){
                 sec = sec.toString().length > 4?sec.toString().substr(0,2):sec.toString().substr(0,1);
                 sec = '0'+ sec;
-                console.log(sec);
+                // console.log(sec);
             }else{
                 sec = '00';
             }
             var min ='0'+  Math.floor(timeGap / (60 * 1000));
-            console.log(min);
+            // console.log(min);
             this.countdownTime = min +':'+sec.substr(-2);
             //迴圈
             if (this.countdownTime !== '00:00') {
                 this.t = setTimeout(() => {
-                    this.runCountDown();
+                    this.runCountDown(key);
                 }, 10);
             } else if (this.countdownTime === '00:00') {
-                if (!this.isRest) {
+                if (!this.stoping) {
                     var vm = this;
-                    this.isRest = true;
-                    vm.runCountDown();
+                    vm.stoping = true;
+                    vm.todos[key].isPlay = false;
+                    vm.countDown(key);
                 } else {
-                    this.isRest = false;
-
+                    this.stoping = false;
                 }
             }
-        }
+        },
     },
 });
 
