ddca53c80deed9115053221b6b0517a187691ffe
nicole0116 <happytu16@gmail.com>
2020-01-05 01:45:27 +0800

    新增change換商品圖

--- js/mall.js
+++ js/mall.js
@@ -1,18 +1,25 @@
 // 小圖換大圖
+// var sImag = document.querySelectorAll('.s_img');
+// var bImags = document.querySelectorAll('.b_img');
+// var bImag = document.querySelectorAll('.b_img')[0];
+
+// for(let i=0;i<sImag.length;i++){
+//   sImag[i].addEventListener("click",(e) => {
+
+//       var img = e.target;
+//       bImag.src = img.src;
+
+//   })
+// }
 
 
-var sImag = document.querySelectorAll('.s_img');
-var bImags = document.querySelectorAll('.b_img');
-var bImag = document.querySelectorAll('.b_img')[0];
 
 
-for(let i=0;i<sImag.length;i++){
-  sImag[i].addEventListener("click",(e) => {
+// 下拉換圖
+let mallColor = document.querySelector('#mall_color');
 
-      var img = e.target;
-      bImag.src = img.src;
-    
-  })
-}
-
-//商品圖放大功能
+mallColor.addEventListener('change', function () {
+  let bImag = document.querySelector('.b_img');
+  let val = this.value;
+  bImag.src = `img/mall_img/${val}.png`;
+})
