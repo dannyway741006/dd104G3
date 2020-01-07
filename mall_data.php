25cfff382f630312a05bbdd5038dd908e0c56970
nicole0116 <happytu16@gmail.com>
2020-01-07 22:12:34 +0800

    no message

---
+++ mall_data.php
@@ -0,0 +1,24 @@
+<?php
+try{
+  require_once("connect.php");
+  $sql = "select * from `mall_product`";
+  $product = $pdo->prepare($sql);
+  // $product->bindValue(":product_type", $_GET["product_type"]);
+  $product->execute();
+  
+  // if( $product->rowCount() == 0 ){ //找不到
+  //   //傳回空的JSON字串
+  //   echo "{}";
+  // }else{ //找得到
+  //   //取回一筆資料
+  //   $productRow = $product->fetchObject();
+  //   // $memRow = $member->fetchObject();  //$memRow->memName
+  //   //送出json字串
+  //   echo json_encode($productRow);
+  // }	
+  echo $product;
+  
+}catch(PDOException $e){
+  echo $e->getMessage();
+}
+?>
