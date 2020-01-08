24314af0e97d5f7142601b65a325838b459ab990
nicole0116 <happytu16@gmail.com>
2020-01-08 22:31:00 +0800

    商城撈資料更新

--- mall_data_test.php
+++ mall_data_test.php
@@ -4,6 +4,7 @@ try{
   $sql = "select * from `mall_product` where product_type=:product_type";
   $product = $pdo->prepare($sql);
   $product->bindValue(":product_type", $_GET["product_type"]);
+  // $product->bindValue(":product_num", $_GET["product_num"]);
   $product->execute();
   
   if( $product->rowCount() == 0 ){ //找不到
@@ -11,7 +12,7 @@ try{
     echo "{}";
   }else{ //找得到
     //取回一筆資料
-    $productRow = $product->fetchObject();
+    $productRow = $product->fetchAll(PDO::FETCH_ASSOC);
     // $memRow = $member->fetchObject();  //$memRow->memName
     //送出json字串
     echo json_encode($productRow);
