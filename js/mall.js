3ef0cff385cb54bcfd518867cc5835cd16b2fae2
nicole0116 <happytu16@gmail.com>
2020-01-09 18:43:51 +0800

    no message

--- js/mall.js
+++ js/mall.js
@@ -81,7 +81,7 @@ $(document).ready(function ($) {
 
 
 let mallBtn01 = document.querySelector('#mall_btn01');
-let mall_data,mall_type,mall_num;
+let mall_data, mall_type, mall_num;
 
 async function getProduct() {
   // fetch('./mall_data.php?product_type=1').then(res=>res.json()).then(json=>console.log(json))
@@ -93,13 +93,13 @@ async function getProduct() {
   mall_content();
 }
 
- 
-  
-   
- 
 
 
-function mall_content(){
+
+
+
+
+function mall_content() {
   var product_name = document.querySelector('#product_name');
   var product_features = document.getElementsByClassName('product_features')[0];
   var product_price = document.querySelector("#product_price");
@@ -111,16 +111,15 @@ function mall_content(){
 
   var mall_color_option = document.createElement("option");
   var mall_color = document.querySelector("#mall_color");
-  var mall_color_sql =  mall_data[0].product_color;
+  var mall_color_sql = mall_data[0].product_color;
 
   // 顏色
   var mall_new_option = mall_color.appendChild(mall_color_option);
-  var mall_option_value_first = mall_data[0].product_src.lastIndexOf("/")+1;
-  var mall_option_value_last = mall_data[0].product_src.lastIndexOf("png")-1;
-  var mall_new_option_value = mall_data[0].product_src.substring(mall_option_value_first,mall_option_value_last);
-  // console.log(mall_new_option_value);
-  mall_new_option.innerHTML = mall_color_sql; 
-  mall_new_option.setAttribute("value",mall_new_option_value);
+  var mall_option_value_first = mall_data[0].product_src.lastIndexOf("/") + 1;
+  var mall_option_value_last = mall_data[0].product_src.lastIndexOf("png") - 1;
+  var mall_new_option_value = mall_data[0].product_src.substring(mall_option_value_first, mall_option_value_last);
+  mall_new_option.innerHTML = mall_color_sql;
+  mall_new_option.setAttribute("value", mall_new_option_value);
 
 
 
@@ -128,92 +127,25 @@ function mall_content(){
   product_features.innerHTML = mall_data[0].product_desc;
   product_name.textContent = mall_data[0].product_name;
   product_price.textContent = mall_data[0].product_price;
-
+  product_name
 
 
   var sbg_arr = [];
-  var product_num = mall_data[0]["product_num"] ;
+  // var sbg_str = [];
+  var product_num = mall_data[0]["product_num"];
   sbg_arr.push(mall_data[0]["product_slide_img"]);
   for (var i in mall_data) {
-
-    if(product_num !== mall_data[i]["product_num"] ){
+    if (product_num !== mall_data[i]["product_num"] ) {
       sbg_arr.push(mall_data[i]["product_slide_img"]);
       product_num = mall_data[i]["product_num"];
     };
   }
-  console.log(sbg_arr); 
-  // if( mall_data.product_no == 101 ){
-  
-
-  // }
-
-
-
-
-
-
-
-
-
-  console.log(mall_data.product_slide_img.length)
-
-
-  // for(var i=0;i<){
-
-  // }
-
-
-
-
+  // console.log(sbg_arr);
+  var owl_img_list = document.querySelectorAll(".owl_img");
+  for (i = 0; i < owl_img_list.length; i++) {
+      document.querySelectorAll(".owl_img")[i].setAttribute("src", sbg_arr[(i+3)%3]);
+  }
 }
-
-// var mallContent = new Vue({
-//     el: '#app',
-//     data: { 
-//         message: 'Hello, Vue!',
-//         score: 0,
-//         colors: ['red','orange','yellow','green','blue','indigo','violet','black'],
-//         classmate: [{name:'Steve',age: 25},{name:'Annie',age: 23},{name:'Tina',age: 28}],
-//     },
-// });
-
-
-
-
-// var mall_content = new Vue({
-//   el: "#app",
-//   data: {
-//     open: false,
-//   },
- 
-//     //顯示商品資料
-//     mallProduct:{
-//         member_name: '王曉明',
-//         userId: 'user3456',
-//         src: './img/program_img/program_member_1.png',
-//         check: '',
-//         uncolor: false,
-//       },
-    
-//   methods: {
-
-
-//   },
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 window.addEventListener("load", getProduct);
