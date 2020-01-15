f1ce3713333465ec7b5127e5767783399ce3d137
gn757541 <gn757541@gmail.com>
2020-01-15 19:42:09 +0800

    no message

---
+++ php_program/push_member.php
@@ -0,0 +1,23 @@
+<?php
+  try{
+    require_once('./connect.php');
+    $sql = "select * from `member`";
+    $member = $pdo->prepare($sql);
+    $member->execute();
+    
+    if( $member->rowCount() == 0 ){ //找不到
+      //傳回空的JSON字串
+      echo "{}";
+    }else{ //找得到
+      //取回一筆資料
+      $memRow = $member->fetch(PDO::FETCH_ASSOC);
+      // $memRow = $member->fetchObject();  //$memRow->memName
+      //送出json字串
+      echo json_encode( $memRow);
+    }  
+    
+  }catch(PDOException $e){
+    echo $e->getMessage();
+  }
+?>
+
