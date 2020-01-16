b27567c03d95ff436ca3d083a475a2e780546342
gn757541 <gn757541@gmail.com>
2020-01-17 03:50:05 +0800

    no message

--- php_program/get_program.php
+++ php_program/get_program.php
@@ -1,24 +1,117 @@
 <?php
+session_start();
+$_SESSION['mem_no'] = 1;//test
 try {
   require_once('./connect.php');
-  $sql = "select c.card_no, c.pro_no, card_name, DATE_FORMAT(`card_date`, '%Y-%m-%d %H:%i') AS `card_date`, card_type, card_sta, t.todo_no, todo_title, file_no, file_name, file_src, todo_cont_no, todo_cont, todo_cont_sta, todo_cont_clock, todo_timer 
-  from `card` c
-  LEFT JOIN `todo` t on c.pro_no = t.pro_no AND c.card_no = t.card_no 
-  LEFT JOIN `card_file` f on f.pro_no = c.pro_no AND f.card_no = c.card_no 
-  LEFT JOIN `todo_content` tc on tc.pro_no = c.pro_no AND tc.todo_no = t.todo_no 
-  WHERE c.pro_no = :pro_no 
+  $sql = "select c.card_no, c.pro_no, jp.pro_mem_no_string, card_name, pic.card_mem_no_string, DATE_FORMAT(`card_date`, '%Y-%m-%d %H:%i') AS `card_date`, card_type, card_sta, t.todo_no, todo_title, file_no, file_name, file_src, todo_cont_no, todo_cont, todo_cont_sta, todo_cont_clock, todo_timer 
+  from `dd104g3`.`card` c
+  LEFT JOIN `dd104g3`.`todo` t on c.pro_no = t.pro_no AND c.card_no = t.card_no 
+  LEFT JOIN `dd104g3`.`card_file` f on f.pro_no = c.pro_no AND f.card_no = c.card_no 
+  LEFT JOIN `dd104g3`.`todo_content` tc on tc.pro_no = c.pro_no AND tc.todo_no = t.todo_no 
+  LEFT JOIN ( SELECT `join_program`.`pro_no`, 
+              GROUP_CONCAT(`join_program`.`mem_no`) AS 'pro_mem_no_string' 
+              FROM `dd104g3`.`join_program` 
+              WHERE `join_program`.`pro_mem_inv` = 1 
+              GROUP BY `join_program`.`pro_no`) jp 
+  ON c.pro_no = jp.pro_no
+  LEFT JOIN ( SELECT `person_in_charge`.`card_no`, 
+              GROUP_CONCAT(`person_in_charge`.`mem_no`) AS 'card_mem_no_string' 
+              FROM `dd104g3`.`person_in_charge` 
+              GROUP BY `person_in_charge`.`card_no`) pic 
+  ON c.card_no = pic.card_no  
+  WHERE c.pro_no = :pro_no
   order by c.card_no";
   $res = $pdo->prepare($sql);
   $res->bindParam(':pro_no', $_POST['pro_no']);
   $res->execute();
   if ($res->rowCount()) {
     $cards = $res->fetchAll(PDO::FETCH_ASSOC);
-    $step0 = array();
-    $step1 = array();
-    $step2 = array();
+    $step0 = array("cards" => array(), "type" => "card_list_todo"); //card_list_todo
+    $step1 = array("cards" => array(), "type" => "card_list_doing"); //card_list_doing
+    $step2 = array("cards" => array(), "type" => "card_list_done"); //card_list_done
     $prevCardId = null;
     $prevTodoId = null;
+    $todo_no_arr = array();
+    $todo_con_no_arr = array();
     foreach ($cards as $card) {
+      switch($card['card_type'])
+      {
+        case "0":
+          if(isset($step0["cards"][$card['card_no']]) == true)
+          {
+            if($card['todo_cont'] != NULL)
+            {
+              $step0["cards"][$card['card_no']]['todo_list_content_detail'][$card['todo_no']]['title'] = $card['todo_title'];
+              if($card['todo_title'] != NULL)
+              {
+                array_push($step0["cards"][$card['card_no']]['todo_list_content_detail'][$card['todo_no']]['lists'], array("content" => $card['todo_cont'], "status" => (int)$card['todo_cont_sta'], "status" => (int)$card['todo_cont_sta'], "text" => (int)$card['todo_cont_sta']));
+              }
+              $step0["cards"][$card['card_no']]['todo_list_content_detail'][$card['todo_no']]['showname'] = false;
+              $step0["cards"][$card['card_no']]['todo_list_content_detail'][$card['todo_no']]['test_title_name'] = true;
+              $step0["cards"][$card['card_no']]['todo_list_content_detail'][$card['todo_no']]['test'] = '';
+          }else{
+            $pro_member_arr = explode(',',$card["pro_mem_no_string"]);
+            $card_member_arr = explode(',',$card["card_mem_no_string"]);
+            if($card['todo_cont'] != NULL)
+            {
+              $todo_list_content_detail_arr[0]['title'] = $card['todo_title'];
+              if($card['todo_title'] != NULL)
+              {
+                $todo_list_content_detail_arr[0]['lists'][0] = array("content" => $card['todo_cont'], "status" => (int)$card['todo_cont_sta'], "status" => (int)$card['todo_cont_sta'], "text" => (int)$card['todo_cont_sta']);
+              }else{
+                $todo_list_content_detail_arr[0]['lists'] = [];
+              }
+              $todo_list_content_detail_arr[0]['showname'] = false;
+              $todo_list_content_detail_arr[0]['test_title_name'] = true;
+              $todo_list_content_detail_arr[0]['test'] = '';
+            }else{
+              $todo_list_content_detail_arr = [];
+            }
+            
+            
+            $step0["cards"][$card['card_no']] = array("card_no"=> $card['card_no'], 
+            "card_name"=> $card['card_name'], 
+            "card_member" => $pro_member_arr, 
+            "showhideMember" => false, 
+            "member_input" => "", 
+            "member_inout" => $card_member_arr, 
+            "todo_list_content_detail" => $todo_list_content_detail_arr, 
+            "dateline" => false, 
+            "dateline_text" => "未完成", 
+            "calendar_date" => "未設定", 
+            "filebox" => array(), 
+            "file_switch" => false);
+          }
+          
+          if($todo_no_arr[$card['card_no']] && $todo_con_no_arr)
+          {
+            array_push($step0["cards"][$todo_no_arr[$card['card_no']]]['todo_list_content_detail'], array('title'=>$card['todo_title'], 'lists' => array()));
+          }else{
+            array_push($step0["cards"],array("card_no"=> $card['card_no'], 
+            "card_name"=> $card['card_name'], 
+            "card_member" => $pro_member_arr, 
+            "showhideMember" => false, 
+            "member_input" => "", 
+            "member_inout" => $card_member_arr, 
+            "todo_list_content_detail" => [], 
+            "dateline" => false, 
+            "dateline_text" => "未完成", 
+            "calendar_date" => "未設定", 
+            "filebox" => [], 
+            "file_switch" => false));
+            $todo_no_arr[$card['card_no']] = count($step0["cards"])-1;
+          }
+          
+          break;
+        case "1":
+          array_push($step1["cards"],array("card_name"=> $card['card_name'], "card_member" => array(), "showhideMember" => false, "member_input" => "", "member_inout" => [], "todo_list_content_detail" => [], "dateline" => false, "dateline_text" => "未完成", "calendar_date" => "未設定", "filebox" => [], "file_switch" => false));
+          break;
+        case "2":
+          array_push($step2["cards"],array("card_name"=> $card['card_name'], "card_member" => array(), "showhideMember" => false, "member_input" => "", "member_inout" => [], "todo_list_content_detail" => [], "dateline" => false, "dateline_text" => "未完成", "calendar_date" => "未設定", "filebox" => [], "file_switch" => false));
+          break;
+        default:
+          break;
+      }
       $contentList = [
         'content' => $card['todo_cont'],
         'id' => $card['todo_cont_no'],
--- php_program/get_program_list.php
+++ php_program/get_program_list.php
@@ -3,35 +3,39 @@
     require_once('./connect.php');
     $sql = 'select * from `program` where mem_no = :mem_no';
     $res = $pdo->prepare($sql);
-    $res->bindParam(':mem_no', $_POST['mem_no']);
+    // $res->bindParam(':mem_no', $_POST['mem_no']);
+    $res->bindValue(':mem_no', 1);
     $res->execute();
     if($res->rowCount() !== 0){
       $projects = $res->fetchAll(PDO::FETCH_ASSOC);
-      $info = array();
+      $info = [];
       foreach($projects as $project){
         $info[] = [
-          'id'=>$project['pro_no'],
-          'info'=>array(
-            'title'=>$project['pro_title'],
-            'color'=>$project['pro_col']
-          ),
-          'list'=>array(
-            array(
-              'status'=>'待辦事項',
-              'todo'=>array()
-            ),
-            array(
-              'status'=>'進行中',
-              'todo'=>array()
-            ),
-            array(
-              'status'=>'已完成',
-              'todo'=>array()
-            )
-          )
+          'pro_no'=>$project['pro_no'],
+          'pro_title'=>$project['pro_title'],
+          'changeimage'=>false,
+          'pro_col'=>$project['pro_col'],
+          'show_complete_info_box'=>'false',
+          'show_delete_info_box'=>'false',
+          'invite_add_member_addr'=>"",
+          'hideMember_sum'=>'false',
+          'program_memeber'=>[],
+          'card_list_todo'=>[
+            'cards'=> [],
+            'type'=> 'card_list_todo'
+          ],
+          'card_list_doing'=>[
+            'cards'=> [],
+            'type'=> 'card_list_doing'
+          ],
+          'card_list_done'=>[
+            'cards'=> [],
+            'type'=> 'card_list_done'
+          ],
+          'pro_sta'=>$project['pro_sta']
         ];
       }
-      echo json_encode(['status'=>'success', 'data'=>$info]);
+      echo json_encode(['status'=>'success', 'data'=> $info]);
     }else{
       echo json_encode(['status'=>'error', 'content'=>'沒有資料']);
     }
