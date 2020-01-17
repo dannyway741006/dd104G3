1efa74784faf229aaa2ee090a5e445a694932b9c
stardussky <stardussky@gmail.com>
2020-01-17 13:48:50 +0800

    update

--- php/mem_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dsn = "mysql:host=localhost;port=3306;dbname=dd104g3;charset=utf8";
-$user = "Yang";
-$password = "T1901166";
-$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-$pdo = new PDO($dsn, $user, $password, $options);
