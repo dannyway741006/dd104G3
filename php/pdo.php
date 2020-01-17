005f645aad788db07a1558a6f4d9a5f1a6d9dd67
stardussky <stardussky@gmail.com>
2020-01-17 22:42:49 +0800

    no message

--- php/pdo.php
+++ php/pdo.php
@@ -1,5 +1,5 @@
 <?php
 $dsn = 'mysql:host=localhost;post=3306;dbname=dd104g3;charset=utf8';
 $options = array([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-$pdo = new PDO($dsn, 'Yang', 'T1601166',$options);
+$pdo = new PDO($dsn, 'Yang', 'T1901166',$options);
 ?>
