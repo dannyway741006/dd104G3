d75b2b8892325f6ce2a603314e70efd4f04da8ce
creativity-07 <kevin8250778@gmail.com>
2020-01-19 22:54:19 +0800

    no message

--- php/pm/card_inner.php
+++ php/pm/card_inner.php
@@ -126,6 +126,19 @@ try {
               
                 echo json_encode(['status' => 'success', 'content' => '更改子項目成功']);
                 break;
+         
+                      
+            case "update_onload_tomato":
+
+                $sql = "update `todo_content` set todo_cont_clock = :todo_cont_clock 
+                where todo_cont_no = :todo_cont_no";
+                $res = $pdo->prepare($sql);
+                $res->bindParam(':todo_cont_clock', $_POST['todo_cont_clock']);
+                $res->bindParam(':todo_cont_no', $_POST['todo_cont_no']);
+                $res->execute();
+              
+                echo json_encode(['status' => 'success', 'content' => '加入番茄鐘']);
+                break;
 
         case "add_file":
             // $pro_no = $_POST['pro_no'];
