6dd004c4aa8fed5702fe139f023c82710195ba73
creativity-07 <kevin8250778@gmail.com>
2020-01-20 22:46:17 +0800

    no message

--- php/pdo.php
+++ php/pdo.php
@@ -1,10 +1,5 @@
 <?php
 $dsn = 'mysql:host=localhost;post=3306;dbname=dd104g3;charset=utf8';
-<<<<<<< HEAD
 $options = array([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-$pdo = new PDO($dsn, 'root', 'nicole0116',$options);
+$pdo = new PDO($dsn, 'root', 'smbobo22',$options);
 ?>
-=======
-$option = array([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-$pdo = new PDO($dsn, 'ezey', 't8me');
->>>>>>> tomato
