f7d26c43d55cfeafbe47e0e36036f55e86b0453a
gav1308 <gav1308@gmail.com>
2020-01-21 15:54:48 +0800

    修正購物清單增加會員編號

--- php/order/order.php
+++ php/order/order.php
@@ -21,9 +21,10 @@ try {
 
   require_once('../pdo.php');
 
-  $sql = "insert into `orders` (`product_price`,`mem_no`,`cret_date`,`ship_addr`,`receiver_name`,`receiver_tel`,`order_sta`) values (:product_price,2, NOW(),:ship_addr,:receiver_name,:receiver_tel,1)";
+  $sql = "insert into `orders` (`product_price`,`mem_no`,`cret_date`,`ship_addr`,`receiver_name`,`receiver_tel`,`order_sta`) values (:product_price,:mem_no, NOW(),:ship_addr,:receiver_name,:receiver_tel,1)";
   $cart = $pdo->prepare($sql);
   $cart->bindValue(":product_price", $_REQUEST["product_price"]);
+  $cart->bindValue(":mem_no", $_REQUEST["mem_no"]);
   $cart->bindValue(":ship_addr", $_REQUEST["ship_addr"]);
   $cart->bindValue(":receiver_name", $_REQUEST["receiver_name"]);
   $cart->bindValue(":receiver_tel", $_REQUEST["receiver_tel"]);
