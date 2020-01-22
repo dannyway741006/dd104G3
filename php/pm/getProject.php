f26bb102a4372035746bcf046f4e5f407310b978
stardussky <stardussky@gmail.com>
2020-01-22 11:13:56 +0800

    no message

--- php/pm/getProject.php
+++ php/pm/getProject.php
@@ -7,11 +7,21 @@ try {
   $cardRes->bindParam('pro_no', $pro_no);
   $cardRes->execute();
   $data = [];
-  $members = [];
   if($cardRes->rowCount()){
     $cards = $cardRes->fetchAll(PDO::FETCH_ASSOC);
     $cardInfo = [];
     foreach($cards as $card){
+      $sql = "select p.mem_no, m.mem_name, m.mem_id, headshot 
+      from `person_in_charge` p
+      JOIN `member` m on m.mem_no = p.mem_no
+      WHERE card_no = {$card['card_no']}";
+      $cardMemberRes = $pdo->prepare($sql);
+      $cardMemberRes->execute();
+      $cardMember = [];
+      if($cardMemberRes->rowCount()){
+        $cardMember = $cardMemberRes->fetchAll(PDO::FETCH_ASSOC);
+      }
+
       $cardInfo[] = [
         'id'=>$card['card_no'],
         'status'=>$card['card_sta'] ? true : false,
@@ -19,7 +29,8 @@ try {
         'deadLine'=>$card['card_date'],
         'type'=>$card['card_type'],
         'files'=>[],
-        'content'=>[]
+        'content'=>[],
+        'cardMember'=>$cardMember
       ];
     }
     $sql = 'select * from `card_file` where pro_no = :pro_no';
@@ -70,6 +81,7 @@ try {
         ];
       }
     }
+
     $step0 = [];
     $step1 = [];
     $step2 = [];
@@ -87,17 +99,7 @@ try {
     }
     $data = [$step0, $step1, $step2];
   }
-  $sql = 'select j.mem_no, m.mem_name, m.mem_id, headshot
-  from `join_program` j
-  JOIN `member` m on m.mem_no = j.mem_no AND pro_mem_inv = 1
-  where pro_no = :pro_no';
-  $memberRes = $pdo->prepare($sql);
-  $memberRes->bindParam('pro_no', $pro_no);
-  $memberRes->execute();
-  if($memberRes->rowCount()){
-    $members = $memberRes->fetchAll(PDO::FETCH_ASSOC);
-  }
-  echo json_encode(['status' => 'success', 'cards' => $data, 'member' => $members]);
+  echo json_encode(['status' => 'success', 'cards' => $data]);
 } catch (PDOException $e) {
   echo $e->getLine();
   echo $e->getMessage();
