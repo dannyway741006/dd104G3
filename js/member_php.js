5b1d845acfb093276cd833f31028ce19b1f55577
stardussky <stardussky@gmail.com>
2020-01-22 16:06:58 +0800

    no message

--- js/member_php.js
+++ js/member_php.js
@@ -2,7 +2,9 @@ function $id(id) {
   return document.getElementById(id);
 }
 const btnLogin = $id("btnLogin");
-btnLogin.addEventListener("click", sendForm);
+const btnSignup = $id('member_sign_up')
+btnLogin.addEventListener("click", login);
+btnSignup.addEventListener('click', signup)
 
 //--------------------顯示登入者資訊
 function showMemInfo(json) {
@@ -19,7 +21,7 @@ function showMemInfo(json) {
 }
 
 // --------------------到server端做登入
-function sendForm() {
+function login() {
   var memId = $id("memId").value;
   var memPsw = $id("memPsw").value;
   //-------------使用ajax方法到Server端資料
@@ -36,4 +38,37 @@ function sendForm() {
   }).then(res=>res.json())
   .then(json=>showMemInfo(json))
   .catch(err=>console.log(err))
+}
+
+function signup(){
+  const memId = $id("memId_s");
+  const memPsw = $id("memPsw_s");
+  const memEmail = $id('memEmail')
+
+  if(memId && memPsw && memEmail){
+    fetch('./php/member/signup.php',{
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({
+        mem_id: memId.value,
+        mem_psw: memPsw.value,
+        mem_email: memEmail.value,
+        headshot,
+        file_type: fileType,
+        file_ext: fileExt
+      })
+    }).then(res=>res.json())
+    .then(json=>{
+      alert(json.content)
+      if(json.status === 'success'){
+        memId.value = ''
+        memPsw.value = ''
+        memEmail.value = ''
+      }
+    })
+    .catch(err=>console.log(err))
+  }
 }
