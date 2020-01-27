57f605db8277cb97b567b96b653406afdc3a031c
stardussky <52324877+stardussky@users.noreply.github.com>
2020-01-27 17:00:10 +0800

    update

--- php/member/isLogin.php
+++ php/member/isLogin.php
@@ -4,7 +4,8 @@
   if(isset($_SESSION['mem_no'])){
     $member = array(
       'mem_no'=>$_SESSION['mem_no'], 'mem_id'=>$_SESSION['mem_id'], 
-      'ring_no'=>$_SESSION['ring_no'], 'mem_name'=>$_SESSION['mem_name'], 'mem_email'=>$_SESSION['mem_email'],
+      'mem_psw'=>$_SESSION['mem_psw'], 'ring_no'=>$_SESSION['ring_no'], 
+      'mem_name'=>$_SESSION['mem_name'], 'mem_email'=>$_SESSION['mem_email'],
       'headshot'=>$_SESSION['headshot'], 'mem_addr'=>$_SESSION['mem_addr'],
       'mem_tel'=>$_SESSION['mem_tel'], 'mem_status'=>$_SESSION['mem_status'],
     );
