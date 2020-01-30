a0c996b2a4bb21a82e7d02b9db166261fdaba026
nicole0116 <happytu16@gmail.com>
2020-01-30 22:35:30 +0800

    no message

--- php/pdo.php
+++ php/pdo.php
@@ -1,10 +1,6 @@
 <?php
 $dsn = 'mysql:host=localhost;post=3306;dbname=dd104g3;charset=utf8';
 $options = array([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-<<<<<<< HEAD
-$pdo = new PDO($dsn, 'Yang', 'T1901166',$options);
-=======
 $pdo = new PDO($dsn, 'root', 'nicole0116',$options);
 
->>>>>>> nicole
 ?>
