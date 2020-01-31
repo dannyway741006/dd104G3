23e4fd67e7b306c3189795a97a11c51f2a0b5d82
ezey-lin <4a252099@gamil.com>
2020-01-31 09:34:49 +0800

    no message

--- php/pdo.php
+++ php/pdo.php
@@ -1,10 +1,4 @@
 <?php
 $dsn = 'mysql:host=localhost;post=3306;dbname=dd104g3;charset=utf8';
-<<<<<<< HEAD
 $option = array([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 $pdo = new PDO($dsn, 'ezey', 't8me');
-=======
-$options = array([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-$pdo = new PDO($dsn, 'root', 'smbobo22',$options);
-?>
->>>>>>> 6dd004c4aa8fed5702fe139f023c82710195ba73
