704c216800ad2436c68448c960c36fa00b3253bf
nicole0116 <happytu16@gmail.com>
2020-02-01 23:09:28 +0800

    購物車title 修正

--- php/pdo.php
+++ php/pdo.php
@@ -1,10 +1,6 @@
 <?php
 $dsn = 'mysql:host=localhost;post=3306;dbname=dd104g3;charset=utf8';
 $options = array([PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-<<<<<<< HEAD
-$pdo = new PDO($dsn, 'ezey', 't8me',$options);
-
-=======
 // <<<<<<< HEAD
 // <<<<<<< HEAD
 // <<<<<<< HEAD
@@ -18,5 +14,4 @@ $pdo = new PDO($dsn, 'ezey', 't8me',$options);
 // =======
 $pdo = new PDO($dsn, 'root', 'nicole0116',$options);
 // >>>>>>> nicole
->>>>>>> nicole
 
