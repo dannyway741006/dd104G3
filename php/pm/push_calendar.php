c7223fec70948aeea5eb7dc98065a58748724a3d
creativity-07 <kevin8250778@gmail.com>
2020-02-02 19:00:42 +0800

    no message

--- php/pm/push_calendar.php
+++ php/pm/push_calendar.php
@@ -8,7 +8,6 @@ try {
       $calendar_cards_arr = [];  //program_memeber
       $calendar_cards_data_arr = [];  //program_memeber
       $sql = " SELECT c.card_no, 
-<<<<<<< HEAD
       c.pro_no, 
       c.card_name, 
       DATE_FORMAT(c.`card_date`, '%Y-%m-%d %H:%i') AS `card_date`, 
@@ -25,22 +24,6 @@ LEFT JOIN `todo_content` tc ON tc.card_no=c.card_no
 LEFT JOIN `join_program` jp ON jp.pro_no=c.pro_no 
 WHERE card_date is not null AND c.card_sta=0 AND p.pro_sta=0 and jp.mem_no=:mem_no
 ORDER BY c.card_date, c.pro_no";
-=======
-                      c.pro_no, 
-                      c.card_name, 
-                      DATE_FORMAT(c.`card_date`, '%Y-%m-%d %H:%i') AS `card_date`, 
-                      c.card_type,
-                      c.card_sta,
-                      p.pro_col, 
-                      p.pro_sta, 
-                      tc.todo_cont_no, 
-                      tc.todo_cont_sta 
-                FROM `card` c 
-                LEFT JOIN `program` p ON c.pro_no=p.pro_no 
-                LEFT JOIN `todo_content` tc ON tc.card_no=c.card_no 
-                WHERE card_date is not null AND c.card_sta=0 AND mem_no=:mem_no 
-                ORDER BY c.card_date, c.pro_no";
->>>>>>> dev
       $res = $pdo->prepare($sql);
       $res->bindValue(':mem_no', $_SESSION["mem_no"]);
       $res->execute();
